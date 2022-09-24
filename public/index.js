78f9edfe325d0cff2bf5402ed431ab245584f32d
Eleila Bradford <eleila.bradford@gmail.com>
2022-09-24 16:23:29 -0600

    initial commit

---
+++ public/index.js
@@ -0,0 +1,15 @@
+const categoryList = document.querySelector("#category-list");
+const newCategoryBtn = document.querySelector("#new-category");
+const newProductBtn = document.querySelector("#new-product");
+
+showProducts = () => {
+
+}
+
+makeCategory = () => {
+
+}
+
+makeProduct = () => {
+
+}
